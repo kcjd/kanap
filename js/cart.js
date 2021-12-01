95f6916252e450741edfea020f844f4f12a00674
Kévin Colonjard <kevincj@outlook.fr>
2021-12-01 19:17:53 +0100

    perf: edit cart functions to avoid unnecessary API calls

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,9 @@ const container = document.querySelector('#cart__items');
 const cartQuantity = document.querySelector('#totalQuantity');
 const cartPrice = document.querySelector('#totalPrice');
 
+// Get cart from local storage
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
 // Get product data from API
 const getProduct = async (id) => {
   try {
@@ -13,42 +16,35 @@ const getProduct = async (id) => {
   }
 };
 
-// Get product data about a cart item
-const getCartItemData = async ({ id, color, quantity }) => {
-  const product = await getProduct(id);
+// Get cart item with product data
+const getCartItemData = async ({ _id, color, quantity }) => {
+  const product = await getProduct(_id);
   return { ...product, color, quantity };
 };
 
 // Delete an item from cart
 const deleteCartItem = ({ id, color }) => {
-  const cart = JSON.parse(localStorage.getItem('cart')) || [];
-  const index = cart.findIndex((i) => i.id === id && i.color === color);
+  const index = cart.findIndex((i) => i._id === id && i.color === color);
   cart.splice(index, 1);
-  localStorage.setItem('cart', JSON.stringify(cart));
+  const cartStorage = cart.map((i) => ({ _id: i._id, color: i.color, quantity: i.quantity }));
+  localStorage.setItem('cart', JSON.stringify(cartStorage));
 };
 
 // Update an item in cart
-const editCartItem = ({ id, color, quantity }) => {
-  const cart = JSON.parse(localStorage.getItem('cart')) || [];
-  const index = cart.findIndex((i) => i.id === id && i.color === color);
+const editCartItemQuantity = ({ id, color, quantity }) => {
+  const index = cart.findIndex((i) => i._id === id && i.color === color);
   cart[index].quantity = quantity;
-  localStorage.setItem('cart', JSON.stringify(cart));
+  const cartStorage = cart.map((i) => ({ _id: i._id, color: i.color, quantity: i.quantity }));
+  localStorage.setItem('cart', JSON.stringify(cartStorage));
 };
 
 // Show cart total on page
-const showCartTotal = async () => {
-  try {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    const cartItems = await Promise.all(cart.map(async (i) => getCartItemData(i)));
+const showCartTotal = () => {
+  const totalQuantity = cart.reduce((total, i) => total + i.quantity, 0);
+  cartQuantity.textContent = `${totalQuantity} ${totalQuantity <= 1 ? 'article' : 'articles'}`;
 
-    const totalQuantity = cartItems.reduce((total, i) => total + i.quantity, 0);
-    cartQuantity.textContent = `${totalQuantity} ${totalQuantity <= 1 ? 'article' : 'articles'}`;
-
-    const totalPrice = cartItems.reduce((total, i) => total + i.price * i.quantity, 0);
-    cartPrice.textContent = totalPrice;
-  } catch (error) {
-    console.error(error.message);
-  }
+  const totalPrice = cart.reduce((total, i) => total + i.price * i.quantity, 0);
+  cartPrice.textContent = totalPrice;
 };
 
 // Get selected item then delete it from cart
@@ -57,8 +53,8 @@ const handleDeleteButton = (e) => {
   const { id, color } = selectedItem.dataset;
 
   selectedItem.remove();
-  deleteCartItem({ id, color });
 
+  deleteCartItem({ id, color });
   showCartTotal();
 };
 
@@ -67,11 +63,10 @@ const handleQuantityInput = (e) => {
   const selectedItem = e.target.closest('[data-id]');
   const { id, color } = selectedItem.dataset;
   const quantity = +e.target.value;
-  const itemQuantity = e.target.previousElementSibling;
 
-  itemQuantity.textContent = `Qté : ${quantity}`;
-  editCartItem({ id, color, quantity });
+  e.target.previousElementSibling.textContent = `Qté : ${quantity}`;
 
+  editCartItemQuantity({ id, color, quantity });
   showCartTotal();
 };
 
@@ -101,12 +96,13 @@ const renderCartItem = ({ _id, name, color, price, quantity, imageUrl, altTxt })
 `;
 
 // Show cart items on page
-const showCartItems = async () => {
+const showCart = async () => {
   try {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
-    const cartItems = await Promise.all(cart.map(async (item) => getCartItemData(item)));
+    cart = await Promise.all(cart.map(async (item) => getCartItemData(item)));
 
-    container.innerHTML = cartItems.map(renderCartItem).join(' ');
+    container.innerHTML = cart.map(renderCartItem).join(' ');
+
+    showCartTotal();
 
     const deleteButtons = document.querySelectorAll('.deleteItem');
     deleteButtons.forEach((button) => button.addEventListener('click', handleDeleteButton));
@@ -118,5 +114,5 @@ const showCartItems = async () => {
   }
 };
 
-document.addEventListener('DOMContentLoaded', showCartItems);
-document.addEventListener('DOMContentLoaded', showCartTotal);
+// Events
+document.addEventListener('DOMContentLoaded', showCart);
